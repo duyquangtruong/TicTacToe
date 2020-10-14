e0cff167c6286029eae387fb68d5f4e75b8ba7b4
Trương Quang Duy <truongquangduy343@gmail.com>
2020-10-14 13:02:53 +0700

    Updates

---
+++ precache-manifest.73271c1ce17c0f38b2cccb392f6b0ec5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffccd597c3efd58224c21bc4539a6916",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "6eb6ac5b6a0600f647d3",
+    "url": "/TicTacToe/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "dbdf6094b99a074ddd8a",
+    "url": "/TicTacToe/static/js/2.094fd168.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TicTacToe/static/js/2.094fd168.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6eb6ac5b6a0600f647d3",
+    "url": "/TicTacToe/static/js/main.84d4aa9e.chunk.js"
+  },
+  {
+    "revision": "35d19f0c13e4574912e8",
+    "url": "/TicTacToe/static/js/runtime-main.29f4516c.js"
+  }
+]);
